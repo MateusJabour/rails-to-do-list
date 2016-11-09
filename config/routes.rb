2e2e4d80324c59540803b6e124375bd24af46c28
Mateus Jabour <mjabourbp@gmail.com>
2016-11-08 20:53:24 -0500

    Sign up actions implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
+
   get '/signup' => 'signup#new'
+  post '/signup' => 'signup#create'
 end
--- test/integration/signup_test.rb
+++ test/integration/signup_test.rb
@@ -5,13 +5,13 @@ class SignupTest < ActionDispatch::IntegrationTest
     visit '/'
     click_link 'Sign up'
 
-    fill_in 'Your name', with: 'John Dow'
+    fill_in 'Your name', with: 'John Doe'
     fill_in 'Your email', with: 'john@example.org'
     fill_in 'Your password', with: 'test'
     fill_in 'Confirm your password', with: 'test'
     click_on 'Create my account'
 
-    asset_equal '/login', page.current_path
+    assert_equal '/login', page.current_path
     assert page.has_text?("Your account has been created.")
   end
 end

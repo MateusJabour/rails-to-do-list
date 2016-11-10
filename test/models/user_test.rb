ee5afb3d2b1146594e8f2ae68df837e58830f933
Mateus Jabour <mjabourbp@gmail.com>
2016-11-10 17:50:54 -0500

    Adds model validation and test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,53 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "requires name" do
+    user = User.create(name: "")
+    refute user.errors[:name].empty?
+  end
+
+  test "requires email" do
+    user = User.create(email: "")
+    refute user.errors[:email].empty?
+  end
+
+  %w[
+    a@a
+    a@a.a
+    invalid
+    a..a@example.com
+    a__a@example.com
+    a.-a@example.com
+    a_-a@example.com
+  ].each do |email|
+    test "requires valid email format (#{email})" do
+      user = User.create(email: email)
+      refute user.errors[:email].empty?
+    end
+  end
+
+  %w[
+    john@example.org
+    john.doe@example.org
+    john-doe@example.org
+    john_doe@example.org
+    john.doe.personal@example.org
+    john+spam@example.org
+    john+spam@sub.domain.com
+    john+spam@sub-domain.com
+    john@me.io
+    john@example.info
+  ].each do |email|
+    test "accepts as valid email format (#{email})" do
+      user = User.create(email: email)
+      assert user.errors[:email].empty?
+    end
+  end
+
+  test "rejects duplicated email" do
+    user = User.create!(name: "John", email:"john@example.org", password: "test")
+    another_user = User.create(email: user.email)
+    
+    refute another_user.errors[:email].empty?
+  end
 end
